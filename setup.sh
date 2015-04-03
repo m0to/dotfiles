fe5e20e75fe7d6d963dc0a37aa8a25914e0f98b0
Jason Liebrecht <jason.liebrecht@gmail.com>
2015-04-02 20:08:24 -0700

    Added alias for sublime

--- setup.sh
+++ setup.sh
@@ -22,3 +22,6 @@ cp "$repo/.gitconfig" "$home/.gitconfig"
 echo "Copied .gitconfig"
 cp "$repo/sublime/Preferences.sublime-settings" "$home/Library/Application Support/Sublime Text 3/Packages/User"
 echo "Copied Sublime Configs"
+
+echo "Making alias for sublime"
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "/usr/local/bin/subl"
